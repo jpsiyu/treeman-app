4436a7b3b0c966a08e76c87e3af63a70b12da9b4
Stephen Wu <jpsiyu@163.com>
2019-05-01 11:11:34 +0800

    code format

--- assets/js/axioswrap.js
+++ assets/js/axioswrap.js
@@ -4,46 +4,37 @@ import qs from 'qs'
 const apiUrl = "http://127.0.0.1:20000"
 
 const get = (path) => {
-    return axios.get(apiUrl + path, getOptions())
-        .catch(handlePromissError)
+  return axios.get(apiUrl + path, getOptions())
+    .catch(handlePromissError)
 }
 
 const put = (path, data) => {
-    return axios.put(apiUrl + path, qs.stringify(data), getOptions())
-        .catch(handlePromissError)
+  return axios.put(apiUrl + path, qs.stringify(data), getOptions())
+    .catch(handlePromissError)
 }
 
 const post = (path, data) => {
-    return axios.post(apiUrl + path, qs.stringify(data), getOptions())
-        .catch(handlePromissError)
+  return axios.post(apiUrl + path, qs.stringify(data), getOptions())
+    .catch(handlePromissError)
 }
 
 const getOptions = () => {
-    if (!window.$nuxt) return {}
-    const options = {
-        headers: {
-            Authorization: window.$nuxt.$store.state.tokenStr
-        }
+  if (!window.$nuxt) return {}
+  const options = {
+    headers: {
+      Authorization: window.$nuxt.$store.state.tokenStr
     }
-    return options
+  }
+  return options
 }
 
 const handlePromissError = (err) => {
-    return
-    const res = err.response
-    if(!res) {
-        console.log("no response from server")
-        return
-    }
-    switch (res.status) {
-        case 401:
-            window.$nuxt.$router.push("/login")
-            break
-    }
+  console.log('catch error', err)
+  return
 }
 
 export default {
-    get,
-    put,
-    post
+  get,
+  put,
+  post
 }
